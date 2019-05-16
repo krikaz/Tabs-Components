1bb0d06ccebc4962c85071de582fdc874764d019
krikaz <pascal.bou@protonmail.com>
2019-05-16 14:31:15 +0200

    working on tabs.js, issue = other tabs are not deselected

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,26 +3,22 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     this.element = element;
-    console.log(this.element);
+    // console.log(this.element);
 
-    
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    console.log(this.data);
-
     
     // Using the custom data attribute get the associated Item element
-    const selector = '.tabs-item[data-tab="${this.data}"]'
-    this.itemElement = document.querySelector(selector);
-    console.log(this.itemElement);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+    // console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem = new TabItem(this.itemElement);
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    // this.tabItem.addEventListener('click', () => {
-    //   this.select();
-    // });
+    this.element.addEventListener('click', () => {
+      this.select();
+    });
 
   };
 
@@ -32,7 +28,7 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    links.forEach(link => {
+    Array.from(links).forEach(link => {
       link.classList.remove('tabs-link-selected');
     });
 
@@ -40,7 +36,7 @@ class TabLink {
     this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
 
   }
 }
@@ -49,6 +45,7 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     this.element = element;
+    console.log(this.element);
   }
 
   select() {
